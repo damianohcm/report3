cd884383c31ec8d17361dc0e3f7b48757ca81048
Damiano Fusco <Damiano.Fusco@tribridge.com>
2016-11-04 15:11:31 -0400

    Code comments

--- app/services/reportServiceConfig.js
+++ app/services/reportServiceConfig.js
@@ -6,11 +6,11 @@
 
         var getConfig = function() {
             var config = {
-                useTestData: true,
-                notApplicableLabel: '0% *',
-                notApplicableIncludeInCalc: true,
-                debug: false,
-                colorPersonSegmentCell: false /* use to drive the addition of css class "with-color" */
+                useTestData: true, /* set to true to load static json data from app/data/ folder instead of using the live API endpoints */
+                notApplicableLabel: '0% *', /* the label used when Learning Objects are Not Applicabile - they are missing from the person los arrays */
+                notApplicableIncludeInCalc: true, /* whether to include the N/A columns in the average aggregated calculation for summary */
+                debug: false, /* true will output additional info in the cells to help identify the code in reportService that populates them */
+                colorPersonSegmentCell: false /* use to drive the addition of css class "with-color" - Dunking does not want them colored but other customers might want it */
             };
 
             return config;
