289746d31081396099df6d814d51f7367ddf78d5
Damiano Fusco <Damiano.Fusco@tribridge.com>
2016-11-16 14:05:22 -0500

    Latest CSS tweaks

--- app/services/reportServiceConfig.js
+++ app/services/reportServiceConfig.js
@@ -9,7 +9,7 @@
 
         var getConfig = function() {
             var config = {
-                useTestData: false, /* set to true to load static json data from app/data/ folder instead of using the live API endpoints */
+                useTestData: true, /* set to true to load static json data from app/data/ folder instead of using the live API endpoints */
                 notApplicableLabel: '0% *', /* the label used when Learning Objects are Not Applicabile - they are missing from the person los arrays */
                 notApplicableIncludeInCalc: true, /* whether to include the N/A columns in the average aggregated calculation for summary */
                 debug: false, /* true will output additional info in the cells to help identify the code in reportService that populates them */
