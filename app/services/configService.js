04233fee6e3f81f3fe608b921305152e61b36de0
Damiano Fusco <Damiano.Fusco@tribridge.com>
2017-01-10 16:19:43 -0500

    Using environment staging for development

--- app/services/configService.js
+++ app/services/configService.js
@@ -10,7 +10,7 @@
 
         // set environment based on where you deploy the code. 
         // Can be 'staging' or 'production'
-        var _environment = 'production';
+        var _environment = 'staging';
 
         var apiBaseUrlStrategy = {
             staging: 'https://dunk-stg.tribridge-amplifyhr.com',
